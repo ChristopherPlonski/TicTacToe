24e21cdab388960165b984b3ed2a8246df6817d0
Christopher Plonski <christopherplonski@gmail.com>
2024-11-12 14:59:28 -0600

    Update HumanPlayer Tests

--- Test_HumanPlayer.cpp
+++ Test_HumanPlayer.cpp
@@ -8,17 +8,17 @@ int main() {
 	cout << "--Testing HumanPlayer." << endl;
 
 	Board board = Board();
-	HumanPlayer humanPlayer = HumanPlayer('X', 1, &board);
+	HumanPlayer humanPlayer = HumanPlayer('X', 1, board);
 
 	assert(humanPlayer.get_marker() == 'X');
 	assert(humanPlayer.get_number() == 1);
 
-	humanPlayer = HumanPlayer('B', 2, &board);
+	humanPlayer = HumanPlayer('B', 2, board);
 	assert(humanPlayer.get_marker() == 'B');
 	assert(humanPlayer.get_number() == 2);
 
-	int inputtedMove = humanPlayer.get_move();
-	cout << "Inputted move: " << inputtedMove << endl;
+	Move inputtedMove = humanPlayer.get_move();
+	cout << "Inputted move: " << inputtedMove.get_optional_mark_position().value() << endl;
 
 	cout << "--HumanPlayer tests have passed!" << endl;
 	return 0;
