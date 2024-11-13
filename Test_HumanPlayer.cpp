dc5785df852135cd7146ec4f8e1dfdbb74d77cd0
Christopher Plonski <christopherplonski@gmail.com>
2024-11-13 15:05:41 -0600

    Update Test_HumanPlayer.cpp

--- Test_HumanPlayer.cpp
+++ Test_HumanPlayer.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cassert>
+
 #include "HumanPlayer.hpp"
+#include "Board.hpp"
 
 using namespace std;
 
@@ -8,12 +10,12 @@ int main() {
 	cout << "--Testing HumanPlayer." << endl;
 
 	Board board = Board();
-	HumanPlayer humanPlayer = HumanPlayer('X', 1, board);
+	HumanPlayer humanPlayer = HumanPlayer('X', 1);
 
 	assert(humanPlayer.get_marker() == 'X');
 	assert(humanPlayer.get_number() == 1);
 
-	humanPlayer = HumanPlayer('B', 2, board);
+	humanPlayer = HumanPlayer('B', 2);
 	assert(humanPlayer.get_marker() == 'B');
 	assert(humanPlayer.get_number() == 2);
 
