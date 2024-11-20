b68cae8a65d67b1960e67df7e219ea8c5f58f428
Christopher Plonski <christopherplonski@gmail.com>
2024-11-20 14:51:03 -0600

    Updated formatting

--- Gamemode.cpp
+++ Gamemode.cpp
@@ -1,24 +1,7 @@
 #include "Gamemode.hpp"
 
 Gamemode::Gamemode(GamemodeType gamemodeType, string gamemodeTitle, string gamemodeDescription) : 
-GAMEMODE_TYPE(gamemodeType), GAMEMODE_TITLE(gamemodeTitle), GAMEMODE_DESCRIPTION(gamemodeDescription)
+	GAMEMODE_TYPE(gamemodeType), GAMEMODE_TITLE(gamemodeTitle), GAMEMODE_DESCRIPTION(gamemodeDescription)
 {
 
 }
-
-//Gamemode::GamemodeType Gamemode::get_gamemode_type() const
-//{
-//	return this->GAMEMODE_TYPE;
-//}
-//
-//string Gamemode::get_gamemode_title() const
-//{
-//	return this->GAMEMODE_TITLE;
-//}
-//
-//string Gamemode::get_gamemode_description() const
-//{
-//	return this->GAMEMODE_DESCRIPTION;
-//}
-
-
