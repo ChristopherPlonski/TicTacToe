3b4747726100bed8f29e2c3ee566652c5aa37f77
Christopher Plonski <christopherplonski@gmail.com>
2024-11-21 12:24:41 -0600

    Update ArchetypeInfoHandler.cpp

--- ArchetypeInfoHandler.cpp
+++ ArchetypeInfoHandler.cpp
@@ -1,4 +1,9 @@
+#include <iostream>
+
 #include "ArchetypeInfoHandler.hpp"
+#include "StringConverter.hpp"
+
+using namespace std;
 
 ArchetypeInfoHandler::ArchetypeInfoHandler() : ARCHETYPE_INFOS_ARRAY{
 	ArchetypeInfo(ArchetypeInfo::ArchetypeType::PYROMANCER, "Pyromancer", "Burn the whole board alive!", "Clear the whole board. Uses up your turn."),
@@ -8,8 +13,44 @@ ArchetypeInfoHandler::ArchetypeInfoHandler() : ARCHETYPE_INFOS_ARRAY{
 
 }
 
-// TODO: THIS
 ArchetypeInfo::ArchetypeType ArchetypeInfoHandler::get_user_to_pick_archetype_type()
 {
-	return ArchetypeInfo::ArchetypeType::PYROMANCER;
+	bool gotValidArchetype = false;
+	const ArchetypeInfo* chosenArchetypeInfo = &ARCHETYPE_INFOS_ARRAY[0]; // Default.
+
+	while (!gotValidArchetype) {
+		cout << "Choose one of the archetypes (type their respective number to choose it).\n";
+
+		for (int i = 0; i < NUMBER_OF_ARCHETYPES; i++) {
+			ArchetypeInfo archetype = ARCHETYPE_INFOS_ARRAY[i];
+			const int NUMBER_REPRESENTING_ARCHETYPE = i + 1;
+
+			cout << "[" << NUMBER_REPRESENTING_ARCHETYPE << "] - " << archetype.ARCHETYPE_NAME << " | " << archetype.ARCHETYPE_DESCRIPTION << endl;
+			cout << "\t - Ability: " << archetype.ARCHETYPE_ABILITY_DESCRIPTION << endl;
+		}
+
+		string userResponse;
+
+		getline(cin, userResponse);
+		optional<int> optionalUserInt = StringConverter::try_get_int_from_string(userResponse);
+
+		if (!optionalUserInt.has_value()) {
+			cout << "Input was not a number. Please type one of the archetype's numbers.\n";
+		}
+		else if (optionalUserInt.value() < 1 || optionalUserInt.value() > NUMBER_OF_ARCHETYPES) {
+			cout << "Input was not one of the displayed archetypes' numbers. Try again.\n";
+		}
+		else {
+			// User input was valid! 
+			int chosenArchetypeIndex = optionalUserInt.value() - 1;
+			chosenArchetypeInfo = &ARCHETYPE_INFOS_ARRAY[chosenArchetypeIndex];
+			gotValidArchetype = true;
+		}
+	}
+
+	cout << "You chose archetype: " << chosenArchetypeInfo->ARCHETYPE_NAME << endl;
+
+	ArchetypeInfo::ArchetypeType chosenArchetypeType = chosenArchetypeInfo->ARCHETYPE_TYPE;
+
+	return chosenArchetypeType;
 }
