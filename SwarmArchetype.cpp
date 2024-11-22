d3f39e1077e8a90560fda8d2494e567abdcaaa7f
JustATortoise <christopherplonski@gmail.com>
2024-11-22 15:48:02 -0600

    Push SwarmArchetype
    Was previously created a while ago, but was not showing up on changed files list until now.

---
+++ SwarmArchetype.cpp
@@ -0,0 +1,6 @@
+#include "SwarmArchetype.hpp"
+
+SwarmArchetype::SwarmArchetype(char playerMarker, int playerNumber, const ArchetypeInfo& archetypeInfo) : ArchetypePlayer(playerMarker, playerNumber, archetypeInfo)
+{
+
+}
---
+++ SwarmArchetype.hpp
@@ -0,0 +1,25 @@
+#ifndef SWARMARCHETYPE_H
+#define SWARMARCHETYPE_H
+
+#include "ArchetypePlayer.hpp"
+
+class SwarmArchetype : public ArchetypePlayer {
+
+public:
+	/// <summary>
+	/// </summary>
+	/// <param name="playerMarker"> From Player base class </param>
+	/// <param name="playerNumber"> From Player base class </param>
+	/// <param name="archetypeInfo"> From ArchetypePlayer base class </param>
+	explicit SwarmArchetype(char playerMarker, int playerNumber, const ArchetypeInfo& archetypeInfo);
+
+	// No need to override this as the ability is passive, so the user doesn't need an extra prompt for it.
+	// string get_move_prompt_text() override;
+
+	// No need to override this as the ability is passive, so the user can't use it (and no need to change the input logic)
+	//pair<optional<Move>, InvalidInput*> try_get_move_from_input(string input) override;
+
+
+};
+
+#endif // !SWARMARCHETYPE_H
