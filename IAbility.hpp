80645f308adba35d7ea6625ab4738e3728db56b4
JustATortoise <christopherplonski@gmail.com>
2024-11-22 00:09:08 -0600

    Updated IAbility
    Added new method.

--- IAbility.hpp
+++ IAbility.hpp
@@ -1,9 +1,17 @@
 #ifndef IABILITY_H
 #define IABILITY_H
 
+#include <utility>
+#include <optional>
+
+#include "InvalidInput.hpp"
+
+using namespace std;
+
 class IAbility {
 
 protected:
+	virtual pair<bool, optional<InvalidInput>> try_use_ability_from_input(string input) = 0;
 	virtual bool can_use_ability() = 0;
 	virtual void activate_ability() = 0;
 };
